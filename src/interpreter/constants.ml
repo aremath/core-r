219c559d1dc9cfc6d30dc0bcc34f6a6428e3286d
AntonXue <antonxue1@gmail.com>
2018-06-07 17:53:08 -0400

    constant folding

--- src/interpreter/constants.ml
+++ src/interpreter/constants.ml
@@ -1,10 +1,6 @@
 
 (* We do the constant folding here *)
 
-(*
-  module R = Syntax
-*)
-
 open Syntax
 module C = Complex
 
@@ -58,10 +54,29 @@ let cast_numeric_pair : numeric * numeric -> castedpair =
     | (Complex c, Float f) -> ComplexPair (c, {C.re=f; C.im=0.0})
     | (Complex c1, Complex c2) -> ComplexPair (c1, c2)
     
+
+let int_of_bool : bool -> int =
+  fun b ->
+    if b then 1 else 0
+
 exception Numeric_Binop_Exception of numeric * numeric * string
+
+let msg_div_by_zero : string =
+  "divide by zero"
+
+let msg_complex_mod : string =
+  "complex modulo is not a supported operation in R"
+
+let msg_complex_int_div : string =
+  "complex integer division is not a supported operation in R"
+
+let msg_complex_comp : string =
+  "complex numbers do not form a naturally ordered set you uncultured swine"
 
 let do_numeric_binop : numbinop -> numeric -> numeric -> numeric =
   fun op n1 n2 -> match (op, cast_numeric_pair (n1, n2)) with
+    | (_, NaPair) -> Na
+
     | (NumPlus, IntPair (i1, i2)) -> Int (i1 + i2)
     | (NumPlus, FloatPair (f1, f2)) -> Float (f1 +. f2)
     | (NumPlus, ComplexPair (c1, c2)) -> Complex (C.add c1 c2)
@@ -74,36 +89,104 @@ let do_numeric_binop : numbinop -> numeric -> numeric -> numeric =
     | (NumMult, FloatPair (f1, f2)) -> Float (f1 *. f2)
     | (NumMult, ComplexPair (c1, c2)) -> Complex (C.mul c1 c2)
 
-
     | (NumDiv, IntPair (i1, i2)) ->
         if i2 != 0 then
           Int (i1 / i2)
         else
-          raise (Numeric_Binop_Exception (n1, n2, "divide by zero"))
+          raise (Numeric_Binop_Exception (n1, n2, msg_div_by_zero))
     | (NumDiv, FloatPair (f1, f2)) ->
         if f2 != 0.0 then
           Float (f1 /. f2)
         else
-          raise (Numeric_Binop_Exception (n1, n2, "divide by zero"))
+          raise (Numeric_Binop_Exception (n1, n2, msg_div_by_zero))
     | (NumDiv, ComplexPair (c1, c2)) ->
         if c2.C.re != 0.0 || c2.C.im != 0.0 then
           Complex (C.div c1 c2)
         else
-          raise (Numeric_Binop_Exception (n1, n2, "divide by zero"))
-
+          raise (Numeric_Binop_Exception (n1, n2, msg_div_by_zero))
 
     | (NumPow, IntPair (i1, i2)) -> Float (float i1 ** float i2)
     | (NumPow, FloatPair (f1, f2)) -> Float (f1 ** f2)
     | (NumPow, ComplexPair (c1, c2)) -> Complex (C.pow c1 c2)
 
+    | (NumMod, IntPair (i1, i2)) -> Int (i1 mod i2)
+    | (NumMod, FloatPair (f1, f2)) -> Float (mod_float f1 f2)
+    | (NumMod, ComplexPair _) ->
+        raise (Numeric_Binop_Exception (n1, n2, msg_complex_mod))
 
+    | (NumIntDiv, IntPair (i1, i2)) ->
+        if i2 != 0 then
+          Int (i1 / i2)
+        else
+          raise (Numeric_Binop_Exception (n1, n2, msg_div_by_zero))
+    | (NumIntDiv, FloatPair (f1, f2)) ->
+        if f2 != 0.0 then
+          Float (floor (f1 /. f2))
+        else
+          raise (Numeric_Binop_Exception (n1, n2, msg_div_by_zero))
+    | (NumIntDiv, ComplexPair _) ->
+        raise (Numeric_Binop_Exception (n1, n2, msg_complex_int_div))
 
+    | (NumGt, IntPair (i1, i2)) -> Int (int_of_bool (i1 > i2))
+    | (NumGt, FloatPair (f1, f2)) -> Int (int_of_bool (f1 > f2))
+    | (NumGt, ComplexPair _) ->
+        raise (Numeric_Binop_Exception (n1, n2, msg_complex_comp))
 
-    (*
+    | (NumGe, IntPair (i1, i2)) -> Int (int_of_bool (i1 >= i2))
+    | (NumGe, FloatPair (f1, f2)) -> Int (int_of_bool (f1 >= f2))
+    | (NumGe, ComplexPair _) ->
+        raise (Numeric_Binop_Exception (n1, n2, msg_complex_comp))
 
-    | (NumMinus, IntPair (i1, i2)) ->
-    | (NumMinus, FloatPair (f1, f2)) ->
-    | (NumMinus, ComplexPair (c1, c2)) ->
+    | (NumLt, IntPair (i1, i2)) -> Int (int_of_bool (i1 < i2))
+    | (NumLt, FloatPair (f1, f2)) -> Int (int_of_bool (f1 < f2))
+    | (NumLt, ComplexPair _) ->
+        raise (Numeric_Binop_Exception (n1, n2, msg_complex_comp))
 
-    *)
+    | (NumLe, IntPair (i1, i2)) -> Int (int_of_bool (i1 <= i2))
+    | (NumLe, FloatPair (f1, f2)) -> Int (int_of_bool (f1 <= f2))
+    | (NumLe, ComplexPair _) ->
+        raise (Numeric_Binop_Exception (n1, n2, msg_complex_comp))
+
+    | (NumEq, IntPair (i1, i2)) -> Int (int_of_bool (i1 = i2))
+    | (NumEq, FloatPair (f1, f2)) -> Int (int_of_bool (f1 = f2))
+    | (NumEq, ComplexPair (c1, c2)) ->
+        Int (int_of_bool (c1.C.re = c2.C.re && c1.C.im = c2.C.im))
+
+    | (NumNeq, IntPair (i1, i2)) -> Int (int_of_bool (i1 != i2))
+    | (NumNeq, FloatPair (f1, f2)) -> Int (int_of_bool (f1 != f2))
+    | (NumNeq, ComplexPair (c1, c2)) ->
+        Int (int_of_bool (c1.C.re != c2.C.re || c1.C.im != c2.C.im))
+
+    | (NumAndVec, IntPair (i1, i2)) ->
+        Int (int_of_bool (i1 != 0 && i2 != 0))
+    | (NumAndVec, FloatPair (f1, f2)) ->
+        Int (int_of_bool (f1 != 0.0 && f2 != 0.0))
+    | (NumAndVec, ComplexPair (c1, c2)) ->
+        Int (int_of_bool ((c1.C.re != 0.0 || c1.C.im != 0.0) &&
+                          (c2.C.re != 0.0 || c2.C.im != 0.0)))
+
+    | (NumAnd, IntPair (i1, i2)) ->
+        Int (int_of_bool (i1 != 0 && i2 != 0))
+    | (NumAnd, FloatPair (f1, f2)) ->
+        Int (int_of_bool (f1 != 0.0 && f2 != 0.0))
+    | (NumAnd, ComplexPair (c1, c2)) ->
+        Int (int_of_bool ((c1.C.re != 0.0 || c1.C.im != 0.0) &&
+                          (c2.C.re != 0.0 || c2.C.im != 0.0)))
+
+    | (NumOrVec, IntPair (i1, i2)) ->
+        Int (int_of_bool (i1 != 0 || i2 != 0))
+    | (NumOrVec, FloatPair (f1, f2)) ->
+        Int (int_of_bool (f1 != 0.0 || f2 != 0.0))
+    | (NumOrVec, ComplexPair (c1, c2)) ->
+        Int (int_of_bool ((c1.C.re != 0.0 || c1.C.im != 0.0) ||
+                          (c2.C.re != 0.0 || c2.C.im != 0.0)))
+
+    | (NumOr, IntPair (i1, i2)) ->
+        Int (int_of_bool (i1 != 0 || i2 != 0))
+    | (NumOr, FloatPair (f1, f2)) ->
+        Int (int_of_bool (f1 != 0.0 || f2 != 0.0))
+    | (NumOr, ComplexPair (c1, c2)) ->
+        Int (int_of_bool ((c1.C.re != 0.0 || c1.C.im != 0.0) ||
+                          (c2.C.re != 0.0 || c2.C.im != 0.0)))
+
 
